e99544bf34d902fa015324c67622b51f6c36bf75
Salil <salil.luley@gmail.com>
2024-03-11 16:20:25 +0000

    Added hero and intro

---
+++ src/app/(marketing)/_components/heading.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import React from "react";
+
+type Props = {};
+
+const Heading = (props: Props) => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your ideas documents and plans unified. Welcome to{" "}
+        <span className="underline">Notes</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Notes is a connected workspace where better ideas are born
+      </h3>
+      <Button>
+        Enter Notes
+        <ArrowRight className="w-4 h-4 ml-2" />
+      </Button>
+    </div>
+  );
+};
+
+export default Heading;
---
+++ src/app/(marketing)/_components/heros.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+type Props = {};
+
+const Heros = (props: Props) => {
+  return (
+    <div className="flex flex-row items-center  max-w-5xl justify-center">
+      <div
+        className="relative
+      w-[300px]
+      h-[300px]
+      sm:w-[350px]
+      sm:h-[350px]
+      md:w-[400px]
+      md:h-[400px]"
+      >
+        <Image
+          src="/documents.png"
+          alt="Documents"
+          fill
+          className="object-contain"
+        ></Image>
+      </div>
+
+      <div className="relative h-[400px] w-[400px] hidden md:block">
+        <Image
+          src="/reading.png"
+          alt="Reading"
+          fill
+          className="object-contain"
+        ></Image>
+      </div>
+    </div>
+  );
+};
+
+export default Heros;
